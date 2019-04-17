3c63137d2d9bcc37509fb13b8a2f84060718b7d6
Rathi <rathind14@gmail.com>
2019-04-17 09:59:35 -0400

    Initial Commit

---
+++ I AM Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by Rathi ND on 4/17/19.
+//  Copyright © 2019 Rathi ND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
